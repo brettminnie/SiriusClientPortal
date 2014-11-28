4d9833b10f7172954d9c9de44004ed1d54cd8de3
Brett Minnie <github@bdb-studios.co.uk>
2014-11-28 14:34:26 +0000

    Initial draft of portal with hard coded stuff

--- src/ministryofjustice/SiriusPortalBundle/Controller/DefaultController.php
+++ src/ministryofjustice/SiriusPortalBundle/Controller/DefaultController.php
@@ -3,17 +3,32 @@
 namespace ministryofjustice\SiriusPortalBundle\Controller;
 
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
+    public function indexAction(Request $request)
     {
-        $client = new Client();
-        /** @var \GuzzleHttp\Message\Response $result */
-        $result = $client->get('http://zf2.be.local/api/person/1',array('headers' => array('X-User-ID' => 'manager@opgtest.com')));
-        $data = json_decode($result->getBody()->getContents(), true);
+        $uid = $request->get('uid', null);
+        $dob = $request->get('dob', null);
 
+        if (!empty($uid) && !empty($dob)) {
+            $client = new Client();
+
+            try {
+                $url = 'http://zf2.be.local/api/my-cases?donor_id=' . $uid . '&dob=' . $dob;
+                /** @var \GuzzleHttp\Message\Response $result */
+                $result = $client->get($url, array('headers' => array('X-User-ID' => 'manager@opgtest.com')));
+                $data = json_decode($result->getBody()->getContents(), true);
+            } catch (RequestException $e) {
+                $data['error'] = json_decode($e->getResponse()->getBody()->getContents(), true);
+            }
+        }
+        else {
+            $data = array();
+        }
         return $this->render('ministryofjusticeSiriusPortalBundle:Default:index.html.twig', $data);
     }
 }
